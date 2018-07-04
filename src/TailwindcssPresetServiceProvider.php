97ad2c8a14692a1a3739521bdd3fd2cd41968cd4
Martin Dilling-Hansen <martindilling@gmail.com>
2018-07-04 22:11:53 +0200

    Offer to run a few commands afterwards

--- src/TailwindcssPresetServiceProvider.php
+++ src/TailwindcssPresetServiceProvider.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace Scriptura\TailwindcssPreset;
 
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\Console\PresetCommand;
 
@@ -17,6 +20,37 @@ class TailwindcssPresetServiceProvider extends ServiceProvider
             TailwindcssPreset::install($command);
             $command->info('Scriptura scaffolding installed successfully.');
             $command->comment('Please run "npm install && npm run dev" or "yarn && yarn run dev" to compile your fresh scaffolding.');
+
+            if ($this->commandExists('yarn')) {
+                $this->offerToRun('yarn && yarn run dev', $command);
+            } else {
+                $this->offerToRun('npm install && npm run dev', $command);
+            }
+
+            $this->offerToRun('composer require barryvdh/laravel-ide-helper', $command);
+            $this->offerToRun('composer require doctrine/dbal', $command);
+            $this->offerToRun('composer require barryvdh/laravel-debugbar', $command);
+            $this->offerToRun('php artisan ide-helper:generate', $command);
+            $this->offerToRun('php artisan ide-helper:meta', $command);
         });
+    }
+
+    private function commandExists(string $command)
+    {
+        $process = new Process("type {$command}");
+        $process->run();
+
+        return $process->isSuccessful();
+    }
+
+    private function offerToRun(string $script, Command $command)
+    {
+        $command->line('');
+        if ($command->confirm("Should I run `{$script}` for you?", true)) {
+            $process = new Process($script);
+            $process->run(function ($type, $buffer) use ($command) {
+                $command->getOutput()->write($buffer);
+            });
+        }
     }
 }
