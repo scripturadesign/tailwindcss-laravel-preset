c5eea5f000f388f3ad1ed13f1027b06e6c4fe97d
Martin Dilling-Hansen <martindilling@gmail.com>
2019-03-18 19:39:20 +0100

    Update paths in webpack mix file

--- src/stubs/webpack.mix.js
+++ src/stubs/webpack.mix.js
@@ -12,8 +12,9 @@ require('laravel-mix-purgecss');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css')
+mix.js('resources/js/app.js', 'public/js');
+
+mix.sass('resources/sass/app.scss', 'public/css')
     .options({
         processCssUrls: false,
         postCss: [
